01050c48ecee64b7f03d4f6e6ca279c2a11cbb8e
Eosoft89 <ericrojas.ing@gmail.com>
2024-09-12 13:46:47 -0300

    Proyect card

--- resources/js/Layouts/MainLayout.tsx
+++ resources/js/Layouts/MainLayout.tsx
@@ -10,14 +10,15 @@ type Props = {
 function MainLayout({children}: Props) {
   return (
     <div>
-        <Navbar expand="lg" className="bg-body-tertiary" fixed="top">
+        <Navbar expand="lg" className="bg-body-secondary" fixed="top">
         <Container>
-            <Link className='navbar-brand' href={route('home')}>Eosoft</Link>
+            <Link className='navbar-brand' href={route('home')}><h4>Eosoft</h4></Link>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-                <NavLink href={route('home')} active={route().current('home')}>Home</NavLink>
-                <NavLink href={route('projects')} active={route().current('projects')}>Projects</NavLink>
+                <NavLink href={route('home')} active={route().current('home')}><h5 className='mr-2'>Inicio</h5></NavLink>
+                <NavLink href={route('projects')} active={route().current('projects')}><h5 className='mr-2'>Proyectos</h5></NavLink>
+                <NavLink href={route('projects')} active={route().current('projects')}><h5 className='mr-2'>Artículos</h5></NavLink>
                 <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
--- resources/js/Pages/Project.tsx
+++ resources/js/Pages/Project.tsx
@@ -1,6 +1,8 @@
 import MainLayout from '@/Layouts/MainLayout';
+import { truncateHTML } from '@/utils/functions';
 import { Head } from '@inertiajs/react';
 import React from 'react';
+import { Button, Card, Col, Row } from 'react-bootstrap';
 
 interface Project {
     id: number;
@@ -16,15 +18,25 @@ function Project({projects}: Props) {
 
     return (
         <MainLayout>
-            <Head title='Project'/>
-            <ul>
+            <Head title='Projects'/>
+            <h2>Proyectos</h2>
+            <Row>
                 {projects.map((project) => 
-                    <li key={project.id}>
-                        <h4>{project.title}</h4>
-                        <p>{project.content}</p>
-                    </li>
+                <Col lg={3} md={6} xs={12} className='bg-success p-1 d-flex justify-content-center align-items-center'>
+                    <Card style={{ width: '20rem', height:'32rem' }} >
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>{project.title}</Card.Title>
+                            <Card.Text>
+                                {truncateHTML(project.content, 100)}
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
                 )}
-            </ul>
+                
+            </Row>
         </MainLayout>
     );
 }
