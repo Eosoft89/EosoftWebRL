7146b68697fe8df33d4794835210007f0503c4aa
Eosoft89 <ericrojas.ing@gmail.com>
2024-10-09 19:02:26 -0300

    Devops

--- resources/js/Pages/Articles.tsx
+++ resources/js/Pages/Articles.tsx
@@ -9,7 +9,7 @@ function Articles({}: Props) {
         <MainLayout>
             <Head title='Articles'/>
             <h2>Articles</h2>
-            <p>No data</p>
+            <p>No data in database</p>
         </MainLayout>
     )
 }
